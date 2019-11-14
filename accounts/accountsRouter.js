a2913659ff66cd5a80b17b0a532d04756b1141b7
Zac Smith <zrs3141592@gmail.com>
2019-11-13 19:48:38 -0700

    create accoutsRouter.js, start CRUD

---
+++ accounts/accountsRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const db = require("../data/dbConfig.js");
+const router = express.Router();
+
+// GET /
+
+// GET /:id
+
+// POST /
+
+// PUT /:id
+
+// DELETE /:id
+
+module.exports = router;
